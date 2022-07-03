7317658de12249a2902ebd255c8c6dea5c4357ba
WrigglySplash <71998907+SoullessCookie@users.noreply.github.com>
2022-07-03 00:23:42 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,124 @@
+import math
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def square_n_times(x, n):
+    return x ** n
+
+def power(x, y):
+    return x ** y
+
+def mod(x, y):
+    return x % y
+
+def root(x, y):
+    return x ** (1/y)
+
+def log(x, y):
+    return math.log(x, y)
+
+def ln(x):
+    return math.log(x)
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)
+
+def asin(x):
+    return math.asin(x)
+
+def acos(x):
+    return math.acos(x)
+
+def atan(x):
+    return math.atan(x)
+
+def sinh(x):
+    return math.sinh(x)
+
+def cosh(x):
+    return math.cosh(x)
+
+def tanh(x):
+    return math.tanh(x)
+
+def asinh(x):
+    return math.asinh(x)
+
+def acosh(x):
+    return math.acosh(x)
+
+def atanh(x):
+    return math.atanh(x)
+
+def factorial(x):
+    return math.factorial(x)
+
+def is_prime(x):
+    return math.is_prime(x)
+
+def is_odd(x):
+    return math.is_odd(x)
+
+def is_even(x):
+    return math.is_even(x)
+
+def is_integer(x):
+    return math.is_integer(x)
+
+def is_real(x):
+    return math.is_real(x)
+
+def is_complex(x):
+    return math.is_complex(x)
+
+def is_finite(x):
+    return math.is_finite(x)
+
+def is_infinite(x):
+    return math.is_infinite(x)
+
+def is_nan(x):
+    return math.is_nan(x)
+
+def is_zero(x):
+    return math.is_zero(x)
+
+def is_positive(x):
+    return math.is_positive(x)
+
+def is_negative(x):
+    return math.is_negative(x)
+
+def is_nonzero(x):
+    return math.is_nonzero(x)
+
+def is_nonpositive(x):
+    return math.is_nonpositive(x)
+
+def is_nonnegative(x):
+    return math.is_nonnegative(x)
+
+def is_subnormal(x):
+    return math.is_subnormal(x)
